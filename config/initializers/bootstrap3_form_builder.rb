e46fc6f6da772b65d6c0036239599155b4e26e95
Zachary Wright <zacharygwright@gmail.com>
2016-05-17 09:55:33 -0400

    Update bootstrap3_form_builder.rb

--- config/initializers/bootstrap3_form_builder.rb
+++ config/initializers/bootstrap3_form_builder.rb
@@ -2,7 +2,7 @@ Bootstrap3FormBuilder.setup do |config|
   # The error messages partial to render with the form
   config.error_partial = "bootstrap3_form_builder/error_messages"
 
-  config.default_submit_style = "btn btn-default"
+  config.default_submit_style = "btn btn-primary"
 
   # The default class to give generated forms.
   # config.default_form_layout = "form-horizontal"
